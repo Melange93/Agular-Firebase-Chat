61115e10f4b7a87d67f41cc94f7cbbfaf7881ac5
Melange93 <lakatos.reka.veronika@gmail.com>
2020-09-28 11:42:22 +0200

    More basic settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChatFormComponent } from './chat-form/chat-form.component';
+import { ChatroomComponent } from './chatroom/chatroom.component';
+import { FeedComponent } from './feed/feed.component';
+import { MessageComponent } from './message/message.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { SignUpFormComponent } from './signup-form/sign-up-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserItemComponent } from './user-item/user-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatFormComponent,
+    ChatroomComponent,
+    FeedComponent,
+    MessageComponent,
+    LoginFormComponent,
+    SignUpFormComponent,
+    NavbarComponent,
+    UserListComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule
